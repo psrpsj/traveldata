110db836d85db9de1eb0d395abc552b14ac1895b
Sangryu Park <psrpsj@gmail.com>
2022-10-11 08:32:22 +0000

    cat2_nlp added

--- argument.py
+++ argument.py
@@ -3,10 +3,11 @@ from transformers import TrainingArguments
 
 
 @dataclass
-class TrainingCat1Arguments(TrainingArguments):
+class TrainingCat1NLPArguments(TrainingArguments):
     output_dir: str = field(default="./output/")
-    num_train_epochs: int = field(default=5)
-    per_device_train_batch_size: int = field(default=8)
+    num_train_epochs: int = field(default=10)
+    per_device_train_batch_size: int = field(default=16)
+    per_device_eval_batch_size: int = field(default=16)
     overwrite_output_dir: bool = field(default=True)
     load_best_model_at_end: bool = field(default=True)
     evaluation_strategy: str = field(default="steps")
@@ -23,8 +24,37 @@ class TrainingCat1Arguments(TrainingArguments):
 
 
 @dataclass
-class TrainCat1ModelArguments:
+class TrainCat1NLPModelArguments:
     model_name: str = field(default="klue/bert-base")
     loss_name: str = field(default="focal")
     project_cat1_name: str = field(default="cat1_nlp")
+    data_path: str = field(default="./data/")
+
+
+@dataclass
+class TrainingCat2NLPArguments(TrainingArguments):
+    output_dir: str = field(default="./output/")
+    num_train_epochs: int = field(default=10)
+    per_device_train_batch_size: int = field(default=16)
+    per_device_eval_batch_size: int = field(default=16)
+    overwrite_output_dir: bool = field(default=True)
+    load_best_model_at_end: bool = field(default=True)
+    evaluation_strategy: str = field(default="steps")
+    save_strategy: str = field(default="steps")
+    metric_for_best_model: str = field(default="accuracy")
+    save_total_limit: int = field(int=5)
+    lr_scheduler_type: str = field(
+        default="cosine_with_restarts",
+        metadata={
+            "help": "Select evaluation strategy[linear, cosine, cosine_with_restarts, polynomial, constant, constant with warmup]"
+        },
+    )
+    warmup_steps: int = field(default=500)
+
+
+@dataclass
+class TrainCat2NLPModelArguments:
+    model_name: str = field(default="klue/bert-base")
+    loss_name: str = field(default="focal")
+    project_cat2_name: str = field(default="cat2_nlp")
     data_path: str = field(default="./data/")
--- train.py
+++ train.py
@@ -3,7 +3,12 @@ import pandas as pd
 import torch
 import wandb
 
-from argument import TrainingCat1Arguments, TrainCat1ModelArguments
+from argument import (
+    TrainingCat1NLPArguments,
+    TrainCat1NLPModelArguments,
+    TrainingCat2NLPArguments,
+    TrainCat2NLPModelArguments,
+)
 from dataset import CustomDataset
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
@@ -26,8 +31,8 @@ def compute_metrics(pred):
     return {"accuracy": acc}
 
 
-def train_cat1():
-    parser = HfArgumentParser((TrainingCat1Arguments, TrainCat1ModelArguments))
+def train_cat1_nlp():
+    parser = HfArgumentParser((TrainingCat1NLPArguments, TrainCat1NLPModelArguments))
     (training_args, model_args) = parser.parse_args_into_dataclasses()
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -84,8 +89,58 @@ def train_cat1():
     print("--- CAT1 NLP TRAINING FINISH ---")
 
 
+def train_cat2_nlp():
+    parser = HfArgumentParser((TrainingCat2NLPArguments, TrainCat2NLPModelArguments))
+    (training_args, model_args) = parser.parse_args_into_dataclasses()
+    device = torch.device("cuda") if torch.cuda.is_avaliable() else torch.device("cpu")
+
+    print("### Training NLP Model for Cat 2 ###")
+    print(f"Current Model is {model_args.model_name}")
+    print(f"Current device is {device}")
+
+    data = pd.read_csv(os.path.join(model_args.data_path, "train.csv"))
+    data["cat2"] = label_to_num(data["cat2"], 2)
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_args.model_name
+    )
+    set_seed(training_args.seed)
+    model_config = AutoConfig.from_pretrained(
+        pretrained_model_name_or_path=model_args.model_name
+    )
+    model_config.num_labels = 18
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_args.model_name, config=model_config
+    )
+    model.resize_token_embeddings(len(tokenizer))
+    model.to(device)
+    model.train()
+
+    wandb.init(
+        entity="psrpsj",
+        project="traveldata",
+        name=model_args.project_cat2_name,
+        tags=model_args.model_name,
+    )
+    wandb.config.update(training_args)
+
+    train_dataset, valid_dataset = train_test_split(
+        data, test_size=0.2, stratify=data["cat2"], random_state=42
+    )
+    train = CustomDataset(train_dataset["overview"], train_dataset["cat2"], tokenizer)
+    valid = CustomDataset(valid_dataset["overview"], valid_dataset["cat2"], tokenizer)
+
+    trainer = CustomTrainer(
+        loss_name - model_args.loss_name,
+        model=model,
+        args=training_args,
+        train_dataset=train,
+        eval_dataset=valid,
+        compute_metrics=compute_metrics,
+    )
+
+
 def main():
-    train_cat1()
+    train_cat1_nlp()
 
 
 if __name__ == "__main__":
