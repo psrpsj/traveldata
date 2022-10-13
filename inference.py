ad3c50750b67b7e158277d32b8277187fff9570e
Sangryu Park <psrpsj@gmail.com>
2022-10-13 08:59:12 +0000

    add inference for cat3 nlp

--- inference.py
+++ inference.py
@@ -4,7 +4,11 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 
-from argument import TrainCat1NLPModelArguments, TrainCat2NLPModelArguments
+from argument import (
+    TrainCat1NLPModelArguments,
+    TrainCat2NLPModelArguments,
+    TrainCat3NLPModelArguments,
+)
 from dataset import CustomDataset
 from torch.utils.data import DataLoader
 from transformers import (
@@ -100,12 +104,63 @@ def inference_cat2_nlp(dataset):
     return dataset
 
 
+def inference_cat3_nlp(dataset):
+    parser = HfArgumentParser(TrainCat3NLPModelArguments)
+    (model_args,) = parser.parse_args_into_dataclasses()
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name)
+    special_tokens_dict = {"additional_special_tokens": ["[RELATION]"]}
+    tokenizer.add_special_tokens(special_tokens_dict)
+
+    overview_fix = (
+        dataset["overview"]
+        + "[RELATION]"
+        + dataset["cat1"]
+        + "[RELATION]"
+        + dataset["cat2"]
+    )
+    dataset["cat3"] = [-1] * len(dataset)
+    test_dataset = CustomDataset(overview_fix, dataset["cat3"], tokenizer)
+    dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+    model_path = os.path.join("./output/", model_args.project_cat2_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    model.resize_token_embeddings(len(tokenizer))
+    model.to(device)
+    model.eval()
+
+    output_prob = []
+    output_pred = []
+    print("### Inference for CAT3 NLP ###")
+    for data in tqdm(dataloader):
+        output = model(
+            input_ids=data["input_ids"].to(device),
+            attention_mask=data["attention_mask"].to(device),
+            token_type_ids=data["token_type_ids"].to(device),
+        )
+        logit = output[0]
+        prob = F.softmax(logit, dim=-1).detach().cpu().numpy()
+        logit = logit.detach().cpu().numpy()
+        result = np.argmax(logit, axis=-1)
+        output_pred.append(result)
+        output_prob.append(prob)
+
+    pred_answer = np.concatenate(output_pred).tolist()
+    output_prob = np.concatenate(output_prob, axis=0).tolist()
+    dataset["cat3"] = num_to_label(pred_answer, 3)
+    submission = pd.DataFrame({"id": dataset["id"], "cat3": dataset["cat3"]})
+    submission.to_csv("./output/submission.csv", index=False)
+    print("### Inference for CAT3 NLP Finish! ###")
+    return dataset
+
+
 def main():
     dataset = pd.read_csv("./data/test.csv")
     if "cat1" not in dataset.columns.tolist():
         dataset = inference_cat1_nlp(dataset)
     if "cat2" not in dataset.columns.tolist():
         dataset = inference_cat2_nlp(dataset)
+    inference_cat3_nlp(dataset)
 
 
 if __name__ == "__main__":
