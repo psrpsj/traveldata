e47ade500a9d71fb33fbc9a3fa896b1beb62f47f
Sangryu Park <psrpsj@gmail.com>
2022-10-14 17:16:11 +0000

    start vision with CNN

--- model.py
+++ model.py
@@ -2,3 +2,34 @@ import numpy
 import torch
 import torch.nn as nn
 
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer = nn.Sequential(
+            # [100, 3, ]
+            nn.Conv2d(
+                in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(
+                in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(
+                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=4, stride=1, padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+    def forward(self, x):
+        output = self.layer(x)
+        output = output.view()
