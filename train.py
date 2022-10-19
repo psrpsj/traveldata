3ee74a0293b2c07bfc454ac9d68e959a5055788c
Sangryu Park <psrpsj@gmail.com>
2022-10-19 15:56:53 +0000

    refact complete, ready for test

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import torch
 import wandb
 
 from argument import (
+    TrainNLPModelArguments,
+    TrainingNLPArguments,
     TrainingCat1NLPArguments,
     TrainCat1NLPModelArguments,
     TrainingCat2NLPArguments,
@@ -31,6 +33,95 @@ def compute_metrics(pred):
     acc = accuracy_score(label, preds)
     f1 = f1_score(label, preds, average="weighted")
     return {"accuracy": acc, "f1_score": f1}
+
+
+def train_nlp(data: pd.DataFrame, cat_num: int):
+    parser = HfArgumentParser((TrainingNLPArguments, TrainNLPModelArguments))
+    (training_args, model_args) = parser.parse_args_into_dataclasses()
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    if cat_num == 1:
+        model_args.project_name = "cat1_nlp"
+        model_args.target_label = "cat1"
+    elif cat_num == 2:
+        model_args.project_name = "cat2_nlp"
+        model_args.target_label = "cat2"
+        data["overview"] = data["overview"] + "[RELATION]" + data["cat1"]
+    elif cat_num == 3:
+        model_args.project_name = "cat3_nlp"
+        model_args.target_label = "cat3"
+        data["overview"] = (
+            data["overview"] + "[RELATION]" + data["cat1"] + "[RELATION]" + data["cat2"]
+        )
+
+    print(f"### Training NLP Model for {model_args.target_label}")
+    print(f"Current model is {model_args.model_name}")
+    print(f"Current device is {device}")
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_args.model_name
+    )
+    special_tokens_dict = {"additional_special_tokens": ["[RELATION]"]}
+    tokenizer.add_special_tokens(special_tokens_dict)
+    set_seed(training_args.seed)
+    model_config = AutoConfig.from_pretrained(
+        pretrained_model_name_or_path=model_args.model_name
+    )
+    if cat_num == 1:
+        model_config.num_labels = 3
+    elif cat_num == 2:
+        model_config.num_labels = 18
+    elif cat_num == 3:
+        model_config.num_labels = 128
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_args.model_name, config=model_config
+    )
+    model.resize_token_embeddings(len(tokenizer))
+    model.to(device)
+    model.train()
+
+    wandb.init(
+        entity="psrpsj",
+        project="traveldata",
+        name=model_args.project_name,
+        tags=[model_args.model_name],
+    )
+    wandb.config.update(training_args)
+    train_dataset, valid_dataset = train_test_split(
+        data, test_size=0.2, stratify=data[model_args.target_label], radom_state=42
+    )
+
+    train_dataset[model_args.target_label] = label_to_num(
+        train_dataset[model_args.target_label], cat_num
+    )
+    valid_dataset[model_args.target_label] = label_to_num(
+        valid_dataset[model_args.target_label], cat_num
+    )
+
+    train = CustomDataset(
+        train_dataset["overivew"], train_dataset[model_args.target_label], tokenizer
+    )
+    valid = CustomDataset(
+        valid_dataset["overview"], valid_dataset[model_args.target_label], tokenizer
+    )
+
+    trainer = CustomTrainer(
+        loss_name=model_args.loss_name,
+        model=model,
+        args=training_args,
+        train_dataset=train,
+        eval_dataset=valid,
+        compute_metrics=compute_metrics,
+    )
+
+    print(f"---{model_args.target_label.upper()} NLP TRAINING ---")
+    trainer.train()
+    model.save_pretrained(
+        os.path.join(training_args.output_dir, model_args.project_name)
+    )
+    wandb.finish()
+    print(f"---{model_args.target_label.upper()} NLP TRAINING FINISH ---")
 
 
 def train_cat1_nlp(data):
@@ -238,10 +329,10 @@ def main():
     dataset = pd.read_csv("./data/train.csv")
     dataset = preprocess_nlp(dataset)
     if not os.path.exists("./output/cat1_nlp"):
-        train_cat1_nlp(dataset)
+        train_nlp(dataset, 1)
     if not os.path.exists("./output/cat2_nlp"):
-        train_cat2_nlp(dataset)
-    train_cat3_nlp(dataset)
+        train_nlp(dataset, 2)
+    train_nlp(dataset, 3)
 
 
 if __name__ == "__main__":
